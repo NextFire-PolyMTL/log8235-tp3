47b1469c6d3b58d07ff4bd9de3b12d34cced55d9
Dominik Courcelles <dominik.courcelles@gmail.com>
2023-12-03 17:10:41 -0500

    Add a comment on the UPROPERTY for the TimeBudget.

--- Source/SoftDesignTraining/AgentBudgetManager.h
+++ Source/SoftDesignTraining/AgentBudgetManager.h
@@ -15,18 +15,17 @@ class SOFTDESIGNTRAINING_API AAgentBudgetManager : public AActor
 public:	
 	AAgentBudgetManager();
 
-public:	
-	// Called every frame
+public:
 	virtual void Tick(float DeltaTime) override;
 
 	void RegisterAgent(ABT_SDTAIController* agent);
 	void UnregisterAgent(ABT_SDTAIController* agent);
 
+	/// The duration in seconds that represents the time spent updating the agents at each frame.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Time Budget", meta = (ClampMin = 0.0))
 	float TimeBudget = 1.0f / 120.0f;
 
 private:
 	TArray<ABT_SDTAIController*> RegisteredAgents;
 	int CurrentIdx = 0;
-	//TArray<ABT_SDTAIController*>::TConstIterator CurrentAgentExecution;
 };
